6a96e6226b6d0c88695691544c6dd404aee42620
AlanT218 <johant218@gmail.com>
2024-11-14 09:41:08 -0500

    Reportes-Vistas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Proyecto_Gestion.Services;
+using Proyecto_Gestion.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,13 @@ namespace Proyecto_Gestion.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ReportService _reportService;
+
+        public HomeController()
+        {
+            _reportService = new ReportService();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -15,10 +24,19 @@ namespace Proyecto_Gestion.Controllers
 
         public ActionResult Report()
         {
-            
             return View();
         }
 
+        public ActionResult Report2()
+        {
+            return View();
+        }
+        public ActionResult DownloadReport()
+        {
+            var reportData = _reportService.GetReportData();
+            return ReportUtility.GeneratePdf(this.ControllerContext, reportData);
+        }
+       
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ Utilities/ReportUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Rotativa;
+using System.Web.Mvc;
+using Proyecto_Gestion.Dtos;
+
+namespace Proyecto_Gestion.Utilities
+{
+    using Rotativa;
+    using System.Web.Mvc;
+
+    public static class ReportUtility
+    {
+        public static ActionResult GeneratePdf(ControllerContext context, ReportDto reportData)
+        {
+            return new ViewAsPdf("Report2", reportData)
+            {
+                FileName = $"Reporte_{DateTime.Now:yyyyMMdd_HHmmss}.pdf",
+                PageSize = Rotativa.Options.Size.A4,
+                PageOrientation = Rotativa.Options.Orientation.Portrait,
+                CustomSwitches = "--footer-right [page]/[toPage] --footer-line --footer-font-size 8 --footer-spacing 10 --footer-font-name Arial"
+            };
+        }
+    }
+}
